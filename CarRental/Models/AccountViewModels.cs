866acca105803a1c0cf3927b057b5d73869dc86a
Sebastian Guzdek <sebag93@gmail.com>
2020-11-13 15:20:01 +0100

    model rejestracji formularz rejestracji

--- CarRental/Models/AccountViewModels.cs
+++ CarRental/Models/AccountViewModels.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CarRental.Models
+﻿namespace CarRental.Models
 {
-    public class AccountViewModels
+
+    #region Rejestracja
+    public class RegisterViewModels
     {
+        //dane do logowania
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        //dane personalne
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Number { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
     }
+    #endregion
 }
