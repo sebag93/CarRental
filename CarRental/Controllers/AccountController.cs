5b1836deed1ea4b870bcaf5f585d0373d22f041f
Sebastian Guzdek <sebag93@gmail.com>
2020-11-13 14:10:39 +0100

    szkielet menu aplikacji dodanie widoków dodanie kontrolerów dodanie modeli

---
+++ CarRental/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarRental/Controllers/RentalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class RentalController : Controller
+    {
+        // GET: Rental/Offer
+        public ActionResult Offer()
+        {
+            return View();
+        }
+
+        // GET: Rental/Fleet
+        public ActionResult Fleet()
+        {
+            return View();
+        }
+    }
+}
