42350c95115eecac97a809e1a0381a1c2257ce3d
Sebastian Guzdek <sebag93@gmail.com>
2020-11-15 18:36:03 +0100

    połączenie z bazą danych kontroler logowania

--- CarRental/Controllers/AccountController.cs
+++ CarRental/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using CarRental.Models;
 
 namespace CarRental.Controllers
 {
@@ -14,6 +16,54 @@ namespace CarRental.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        // POST: Account/Login
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel dane, string ReturnUrl = "")
+        {
+            string message = "";
+            using (CarRentalEntities db = new CarRentalEntities())
+            {
+                var v = db.Users.Where(x => x.email == dane.Email && x.password == dane.Password).FirstOrDefault();
+                if (v != null)
+                {
+                    int timeout = dane.RememberMe ? 525600 : 20; //525600 minut to 1 rok
+                    var ticket = new FormsAuthenticationTicket(dane.Email, dane.RememberMe, timeout);
+                    string encrypted = FormsAuthentication.Encrypt(ticket);
+                    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted)
+                    {
+                        Expires = DateTime.Now.AddMinutes(timeout),
+                        HttpOnly = true
+                    };
+                    Response.Cookies.Add(cookie);
+                    if (Url.IsLocalUrl(ReturnUrl))
+                    {
+                        //return RedirectToAction(ReturnUrl);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    message = "Wprowadzono niepoprawne dane";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
+
+        //Logout
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
         }
 
         // GET: Account/Register
